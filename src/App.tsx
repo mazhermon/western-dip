d16159aa4c5c8ba66e3acde9249a7d37805c6834
Maz Hermon <mazhermon@gmail.com>
2022-01-08 21:01:29 +1300

    home page changes to test delpoy pipeline early

--- src/App.tsx
+++ src/App.tsx
@@ -11,38 +11,14 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p className="header">
-          🚀 Vite + React + Typescript 🤘 & <br />
-          Eslint 🔥+ Prettier
-        </p>
+        <p className="header">Western Dip</p>
 
         <div className="body">
           <button onClick={() => setCount((count) => count + 1)}>
-            🪂 Click me : {count}
+            Click me test : {count}
           </button>
 
-          <p> Don&apos;t forgot to install Eslint and Prettier in Your Vscode.</p>
-
-          <p>
-            Mess up the code in <code>App.tsx </code> and save the file.
-          </p>
-          <p>
-            <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer">
-              Learn React
-            </a>
-            {' | '}
-            <a
-              className="App-link"
-              href="https://vitejs.dev/guide/features.html"
-              target="_blank"
-              rel="noopener noreferrer">
-              Vite Docs
-            </a>
-          </p>
+          <p>Make a website already</p>
         </div>
       </header>
     </div>
